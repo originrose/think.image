7b8d83d785bf4c2bcc384273494aa7c3d6e3dbf9
Chris Nuernberger <chris@thinktopic.com>
2016-11-30 15:07:34 -0700

    0.4.2 Release.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject thinktopic/think.image "0.4.2-SNAPSHOT"
+(defproject thinktopic/think.image "0.4.2"
   :description "Image manipulation library"
   :url "http://github.com/thinktopic/think.image"
   :license {:name "Eclipse Public License"
