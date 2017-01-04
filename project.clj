7ad57efbfc979a780d62937bd4860155dfbaf3d3
Chris Nuernberger <chris@thinktopic.com>
2017-01-04 09:52:42 -0700

    Bumping back to snapshot.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject thinktopic/think.image "0.4.4"
+(defproject thinktopic/think.image "0.4.5-SNAPSHOT"
   :description "Image manipulation library"
   :url "http://github.com/thinktopic/think.image"
   :license {:name "Eclipse Public License"
