ff99c68bb6e1f0d5f747ab60afcd90484e3ffaeb
bburdette <bburdette@gmail.com>
2017-01-17 16:35:39 -0700

    check that source rect is within source image

--- test/clj/think/image/data_augmentation_test.clj
+++ test/clj/think/image/data_augmentation_test.clj
@@ -3,6 +3,7 @@
                                                    random-rect-mog 
                                                    xform-point
                                                    random]]
+            [clojure.pprint :as pp]
             [clojure.test :refer :all]
             ))
 
@@ -12,28 +13,46 @@
 (defn- dbleq [a b]
   (< (Math/abs (- a b)) 0.01))
 
+(defn- within-rect? [x1 y1 x2 y2 x y]
+  (let [x (Math/round x)
+        y (Math/round y)
+        res (and (<= x1 x)
+                 (<= x x2)
+                 (<= y1 y)
+                 (<= y y2))]
+    (if res
+      res
+      (do 
+        (println "within-rect fail: " [x1 y1 x2 y2 x y])
+        res))))
+
 (defn test-mog-pts [m]
   (let [tx (rect-mog->affinetransform m)
         itx (.createInverse tx)
+        [sw sh] (:source-dims m)
         tpoints (apply corner-points (:target-dims m))
         rcorners (map (partial xform-point tx) (:corners m))
         invpoints (map (partial xform-point itx) tpoints)]
-    ; check xforming the target image dims to the corner dims.
-    ; also the corner points back to the target dims.  
-    (and (every? identity (map dbleq (apply concat tpoints) (apply concat rcorners)))
-         (every? identity (map dbleq (apply concat (:corners m)) (apply concat invpoints))))))
+    (and ; all source rect points should be within the source image.
+         ; (every? identity (map (fn [[x y]] (within-rect? 0 0 sw sh x y)) invpoints))
+         (every? identity (map (fn [[x y]] (within-rect? 0 0 sw sh x y)) (:corners m)))
+         ; check xforming the target image dims to the corner dims.
+         (every? identity (map dbleq (apply concat tpoints) (apply concat rcorners)))
+         ; also the corner points back to the target dims. 
+         (every? identity (map dbleq (apply concat (:corners m)) (apply concat invpoints)))
+         )))
 
 (defn test-many-mogs [n]
-  (doall (map test-mog-pts 
-              (map (fn [_] (random-rect-mog [(random 10 4000) (random 10 4000)]
-                                            [(random 10 4000) (random 10 4000)]
+  (let [mogs (map (fn [_] (random-rect-mog [(Math/floor (random 10 4000)) (Math/floor (random 10 4000))]
+                                            [(Math/floor (random 10 4000)) (Math/floor (random 10 4000))]
                                             true
                                             true
                                             (random 0.1 100.0)
                                             1.0))
-                   (repeat n 0)))))
+                   (repeat n 0))]
+    (doall (map test-mog-pts mogs))))
 
 (deftest rect-mog-sanity-check []
-  (is (test-many-mogs 100)))
+  (is (every? identity (test-many-mogs 100))))
 
 
