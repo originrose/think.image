5f8b0fdace59c5f40bfdfe71ab80f47b6277e93c
Chris Nuernberger <chris@thinktopic.com>
2017-04-10 15:56:56 -0600

    Adding ability to affine scale without doing any other transforms because being able to set the interpolation type is useful.

--- src/clj/think/image/data_augmentation.clj
+++ src/clj/think/image/data_augmentation.clj
@@ -198,8 +198,8 @@
 (def ^{:private true} pi 3.141592653589793)
 (def ^{:private true} tau (* 2.0 pi))
 
-(defn- rot 
-  "Produce a rotation matrix to rotate a 2D vector by a given angle, 
+(defn- rot
+  "Produce a rotation matrix to rotate a 2D vector by a given angle,
   where the angle is in 'turns', i.e. 180 degrees = 0.5)"
   ([turns]
     (let [a (* tau turns)]
@@ -231,45 +231,45 @@
   "build a rect-tx-recipe for use with random-rect-tx.
   source-dims are required, but its handy to build a recipe without these
   if you won't know the source resolution/filename until later."
-  { :source-dims source-dims  ; [width height] 
-    :source-filename source-filename 
+  { :source-dims source-dims  ; [width height]
+    :source-filename source-filename
     :target-dims target-dims  ; [width height]
     :flip-x flip-x
     :flip-y flip-y
-    :lower-scaling-limit lower-scaling-limit    
+    :lower-scaling-limit lower-scaling-limit
     :max-angle max-angle })
 
 (defn add-rect-tx-source-info [rect-tx
-                               [source-width source-height]              
+                               [source-width source-height]
                                & {:keys [source-filename] :or { :source-filename nil }}]
   "add the source image info - resolution and optional filename."
-  (assoc-if rect-tx :source-dims [source-width source-height]              
+  (assoc-if rect-tx :source-dims [source-width source-height]
                     :source-filename source-filename))
 
 
 
 (defn random-rect-tx [{:keys [source-filename
-                              source-dims 
-                              target-dims 
-                              flip-x 
-                              flip-y 
-                              lower-scaling-limit 
+                              source-dims
+                              target-dims
+                              flip-x
+                              flip-y
+                              lower-scaling-limit
                               max-angle]}]
   "random rectangle transformation, aka random-rect-tx.
-  Generate a specification for creating a target image from a rectangle within a source image.  
-  The rectangle in the source is positioned by a random rotation, translation, scaling, and 
-  x/y flip.  
+  Generate a specification for creating a target image from a rectangle within a source image.
+  The rectangle in the source is positioned by a random rotation, translation, scaling, and
+  x/y flip.
   Parameters:
   source-dims: [source-width source-height] - dimensions of the source image.
   target-dims: [target-width target-height] - dimensions of the target image.
   flip-x : if true, 50% chance of an x-flip.
   flip-y : if true, 50% chance of a y-flip.
-  lower-scaling-limit : This is a ratio of source rect size to target rect size. 
+  lower-scaling-limit : This is a ratio of source rect size to target rect size.
                         The randomly generated scale factor will be between this and the largest
-                        possible value. 
+                        possible value.
                         for instance 0.5 means allow 128x128 source rect for 256x256 target.
                         2.0 means only allow larger than 512x512 for source rect for 256x256 target.
-      if lower-scaling-limit is too large, for instance 5.0 for a 100x100 target and 400x400 source, 
+      if lower-scaling-limit is too large, for instance 5.0 for a 100x100 target and 400x400 source,
       then the largest possible scale factor for a given rotation is used.
   max-angle: maximum rotation (in 'turns'!) allowed in constructing source rect.
              for example:  0.0 means no rotation allowed.
@@ -285,15 +285,15 @@
         xes [0 (first rx) (+ (first rx) (first ry)) (first ry)]
         yes [0 (second rx) (+ (second rx) (second ry)) (second ry)]
         ; min and max of those points.
-        minx (reduce min xes) 
-        maxx (reduce max xes) 
-        miny (reduce min yes) 
+        minx (reduce min xes)
+        maxx (reduce max xes)
+        miny (reduce min yes)
         maxy (reduce max yes)
         xw (- maxx minx)
         yh (- maxy miny)
         xes (map #(- % minx) xes)
         yes (map #(- % miny) yes)
-        ; largest scale factor possible with our current rotation. 
+        ; largest scale factor possible with our current rotation.
         upper-scale (min (/ source-width xw) (/ source-height yh))
         ; actual scale factor we'll use.
         scale-factor (if (< lower-scaling-limit upper-scale)
@@ -303,26 +303,26 @@
         xes (map (partial * scale-factor) xes)
         yes (map (partial * scale-factor) yes)
         ; calc random translation.
-        xrange (- source-width (* xw scale-factor)) 
+        xrange (- source-width (* xw scale-factor))
         yrange (- source-height (* yh scale-factor))
         xt (random 0 xrange)
-        yt (random 0 yrange) 
+        yt (random 0 yrange)
         ; apply random translation to test points.
         xes (map (partial + xt) xes)
         yes (map (partial + yt) yes)
         ; apply xflip if needed.
         xflip (if flip-x (if (> (rand) 0.5) true false) false)
         [xes yes] (if xflip
-                    (let [[x0 x1 x2 x3] xes  
-                          [y0 y1 y2 y3] yes] 
+                    (let [[x0 x1 x2 x3] xes
+                          [y0 y1 y2 y3] yes]
                       ; swap 0,1 and 2,3
                       [[x1 x0 x3 x2] [y1 y0 y3 y2]])
                     [xes yes])
         ; yflip if needed.
         yflip (if flip-y (if (> (rand) 0.5) true false) false)
         [xes yes] (if yflip
-                    (let [[x0 x1 x2 x3] xes 
-                          [y0 y1 y2 y3] yes] 
+                    (let [[x0 x1 x2 x3] xes
+                          [y0 y1 y2 y3] yes]
                       ; swap 0,3 and 1,2
                       [[x3 x2 x1 x0] [y3 y2 y1 y0]])
                     [xes yes])
@@ -334,32 +334,52 @@
                 :source-dims [source-width source-height]
                 :target-dims [target-width target-height]
                 :xflip xflip                  ; whether to flip the image in x (map c+x to c-x, where c is the image center)
-                :yflip yflip                  ; whether to flip the image in y 
+                :yflip yflip                  ; whether to flip the image in y
                 :corners (map list xes yes) } ; corners of the source rectangle, for sanity checking.
           :source-filename source-filename))) ; add in the source-filename if its not nil.
 
 (defn rect-tx->affinetransform ^AffineTransform [{:keys [translate1 translate2 xflip yflip angle scale target-dims]}]
   (let [scale (/ 1.0 scale)
         [width height] target-dims
-        [xt1 yt1] translate1 
-        [xt2 yt2] translate2 
+        [xt1 yt1] translate1
+        [xt2 yt2] translate2
         [xc yc] [(quot width 2) (quot height 2)]
         tx (AffineTransform.)]
-    (if xflip
-      (do (.translate tx (* width 0.5) 0)
-          (.scale tx -1.0 1.0) 
-          (.translate tx (- (* width 0.5)) 0)))
-    (if yflip
-      (do 
-        (.translate tx 0 (* height 0.5))
-        (.scale tx 1.0 -1.0) 
-        (.translate tx 0 (- (* height 0.5)))
-        ))
+    (when xflip
+      (.translate tx (* width 0.5) 0)
+      (.scale tx -1.0 1.0)
+      (.translate tx (- (* width 0.5)) 0))
+    (when yflip
+      (.translate tx 0 (* height 0.5))
+      (.scale tx 1.0 -1.0)
+      (.translate tx 0 (- (* height 0.5))))
     (.rotate tx (* angle tau) 0 0)
     (.translate tx xt1 yt1)
     (.scale tx scale scale)
     (.translate tx xt2 yt2)
-    tx)) 
+    tx))
+
+
+(defn- interp->affine-op
+  ^long [interp]
+  (condp = interp
+    :bilinear AffineTransformOp/TYPE_BILINEAR
+    :nearest-neighbor AffineTransformOp/TYPE_NEAREST_NEIGHBOR
+    AffineTransformOp/TYPE_BICUBIC))
+
+
+(defn affine-scale
+  ^BufferedImage [^BufferedImage src scale-x scale-y & {:keys [interpolation]}]
+  (let [tx (AffineTransform.)
+        src-width (think-image/width src)
+        src-height (think-image/height src)
+        out-image (i/new-image (long (* src-width (double scale-x)))
+                               (long (* src-height (double scale-y))))
+        _ (.scale tx scale-x scale-y)
+        op (AffineTransformOp. tx (interp->affine-op interpolation))]
+    (.filter op src out-image)
+    out-image))
+
 
 (defn xform-point [^AffineTransform tx [x y]]
   "transform an [x y] point with the AffineTransform and return the result."
@@ -368,25 +388,30 @@
     (.transform tx from to)
     [(.getX to) (.getY to)]))
 
-(defn rect-tx-image  
-  "given a source image and a 'rect-tx', return a new image according to the 
+
+(defn rect-tx-image
+  "given a source image and a 'rect-tx', return a new image according to the
   rect-tx parameters, with width and height as in :target-dims.
-  optional is the affinetransform interpolation type.  Use TYPE_NEAREST_NEIGHBOR 
-  for masks, where you don't want interpolated colors."
-  (^BufferedImage [^BufferedImage img 
-                  {:keys [target-dims] :as tx}
-                  ato]   ; AffineTransformOp/TYPE_BICUBIC, TYPE_BILINEAR, or TYPE_NEAREST_NEIGHBOR
+  optional is the affinetransform interpolation type.
+  Interpolation is one of:
+  :bicubic
+  :bilinear
+:nearest-neighbor."
+  (^BufferedImage [^BufferedImage img
+                   {:keys [target-dims] :as tx}
+                   & {:keys [interpolation]
+                      :or {interpolation :bicubic}}]
     (let [[width height] target-dims
           tx (rect-tx->affinetransform tx)
           out-image (i/new-image width height)
-          op (AffineTransformOp. tx AffineTransformOp/TYPE_BICUBIC)]
+          op (AffineTransformOp. tx (interp->affine-op interpolation))]
       (.filter op img out-image)
       out-image))
- (^BufferedImage [^BufferedImage img 
+ (^BufferedImage [^BufferedImage img
                  tx]
    (rect-tx-image img tx AffineTransformOp/TYPE_BICUBIC)))
 
-(defn draw-point-lines [^BufferedImage img 
+(defn draw-point-lines [^BufferedImage img
                          points
                          color]
   "draw lines through the sequence of points, and from the last point back to the first."
@@ -401,11 +426,11 @@
            p+1))))
 
 ; ----------------------------------------------------------------------------------
-; some handy functions for verifying visually that the rect-tx is working, or if 
+; some handy functions for verifying visually that the rect-tx is working, or if
 ; not, how it isn't working.
 ; ----------------------------------------------------------------------------------
 
-(defn draw-tx-box [^BufferedImage img 
+(defn draw-tx-box [^BufferedImage img
                         {:keys [corners] :as tx}
                         color]
   (draw-point-lines img corners color))
@@ -425,7 +450,7 @@
     (i/write img (str filename ".box.png") "png")
     mp))
 
-(defn test-image-nx [filename [target-width target-height] n] 
+(defn test-image-nx [filename [target-width target-height] n]
   (let [img (i/load-image filename)
         mpis (map (fn [i] [(random-rect-tx [(.getWidth img) (.getHeight img)] [target-width target-height] true true 0.3 0.1)
                           i])
@@ -433,40 +458,40 @@
     (doall
       (map (fn [[mp i]]
              (i/write (rect-tx-image img mp) (str filename i ".out.png") "png"))
-           mpis)) 
+           mpis))
     mpis
     ))
 
 ; ----------------------------------------------------------------------------------
-; random image-and-mask rects.   
-; given a directory of images (and masks), generate a series of rect-tx structs, each associated 
-; with a specific image.  
-; the images may be of varying resolutions, so source resolution is left out of the 
-; rect-tx-recipe.  
+; random image-and-mask rects.
+; given a directory of images (and masks), generate a series of rect-tx structs, each associated
+; with a specific image.
+; the images may be of varying resolutions, so source resolution is left out of the
+; rect-tx-recipe.
 ; ----------------------------------------------------------------------------------
 
 (defn load-image-w-helpful-exception ^BufferedImage [file]
   (try
     (i/load-image file)
-    (catch Exception e 
+    (catch Exception e
       (println "exception opening file: " file)
       (println "msg: " (.getMessage e)))))
 
-(defn rect-tx-w-mask [^BufferedImage image 
-                      ^BufferedImage mask 
+(defn rect-tx-w-mask [^BufferedImage image
+                      ^BufferedImage mask
                       mask-color->category-index
                       {:keys [target-dims] :as rect-tx }]
   (let [image-patch (rect-tx-image image rect-tx)
         [target-width target-height] target-dims
-        ; nearest neighbor interpolation so that we don't get invalid mask colors. 
+        ; nearest neighbor interpolation so that we don't get invalid mask colors.
         mask-patch (rect-tx-image mask rect-tx AffineTransformOp/TYPE_NEAREST_NEIGHBOR)
         [mask-width mask-height] [(.getWidth mask-patch) (.getHeight mask-patch)]
         labels (m/new-matrix :vectorz mask-height mask-width)]
-    (doall 
-      (for [x (range 0 mask-width) 
+    (doall
+      (for [x (range 0 mask-width)
             y (range 0 mask-height)]
         (m/mset! labels y x (mask-color->category-index (c/components-rgb (.getRGB mask-patch x y))))))
-    {:labels labels 
+    {:labels labels
      :final-img image-patch
      :rect-tx rect-tx }))
 
@@ -493,14 +518,14 @@
 
 
 (defn replay-rtx-w-mask [image-dir mask-dir mask-color->category-index rect-tx]
-  "rebuild a training image and mask, given a rect-tx and the appropriate 
+  "rebuild a training image and mask, given a rect-tx and the appropriate
   image directories (and a color->index function)"
   (let [{:keys [source-filename
-                source-dims 
-                target-dims 
-                flip-x 
-                flip-y 
-                lower-scaling-limit 
+                source-dims
+                target-dims
+                flip-x
+                flip-y
+                lower-scaling-limit
                 max-angle]} rect-tx
         ^java.io.File image-file (clojure.java.io/file image-dir source-filename)
         ^java.io.File mask-file (clojure.java.io/file mask-dir (str source-filename "-mask.png"))
@@ -508,9 +533,5 @@
         mask (load-image-w-helpful-exception mask-file)
         image-dims [(.getWidth image) (.getHeight image)]]
     (if (not= source-dims image-dims)
-      { :fail "image dimensions don't match!" } 
+      { :fail "image dimensions don't match!" }
       (rect-tx-w-mask image mask mask-color->category-index rect-tx))))
-
-                                         
-
-
