5e6518bd1564734b6174609a93fb9bd7b01d42ce
Chris Nuernberger <chris@thinktopic.com>
2017-05-03 13:07:40 -0600

    Upgrade datatype dependency.

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [thinktopic/think.datatype "0.3.10"]
+                 [thinktopic/think.datatype "0.3.11"]
                  [net.mikera/imagez "0.12.0"]]
 
   :source-paths ["src/clj" "src/cljc"]
@@ -12,9 +12,11 @@
   :plugins [[lein-codox "0.10.2"]]
 
   :java-source-paths ["java"]
+
   :profiles { :uberjar { :aot :all }
              :dev {:source-paths ["test/shared"
                                   "test/clj"] }
              :test {:source-paths ["test/shared"
                                    "test/clj"] }}
+
   :think/meta {:type :library :tags [:image :clj :cljc]})
