bba414db19193bb0a3843ff2d50c01a78dd4a2e5
Chris Nuernberger <chris@thinktopic.com>
2017-08-29 10:49:56 -0600

    Release 0.4.14

--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
-(defproject thinktopic/think.image "0.4.14-SNAPSHOT"
+(defproject thinktopic/think.image "0.4.14"
   :description "Image manipulation library"
   :url "http://github.com/thinktopic/think.image"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0-alpha17"]
-                 [thinktopic/think.datatype "0.3.15"]
+                 [thinktopic/think.datatype "0.3.16"]
                  [net.mikera/imagez "0.12.0"]]
 
   :source-paths ["src/clj" "src/cljc"]
